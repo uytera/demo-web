0fec1c6b7346a4a938c43e9a125a52a0f64d2de0
uytera <uytera12@yandex.ru>
2019-09-07 14:25:35 +0500

    Шаблонизатор

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -1,6 +1,7 @@
 package com.example.demoweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,9 +13,11 @@ public class PostsViewController {
 
     @ResponseBody
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    public String list(Model model) {
+        model.addAttribute("appName", "Сентябрь горит");
+        return "Моё супер приложение";
     }
+
 
     @ResponseBody
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
